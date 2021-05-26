d5172e4d1be89fb379d395036055828054a8b12c
UMH_IIN <allanlopezc1994@gmail.com>
2021-05-25 18:47:28 -0600

    Hooks y Props por defecto

--- Ejercicios React/proyecto1/src/App.js
+++ Ejercicios React/proyecto1/src/App.js
@@ -1,12 +1,38 @@
+import React, {useEffect, useState} from "react";
 import './App.css';
 import Persona from "./components/Persona";
 
 function App() {
 
+  //Hook de estado
+  const [estadoPersona, setEstadoPersona] = useState(false);
+  var estado;  
+  
+  if(estadoPersona === true ){
+    estado ="Persona Habilitada";
+  }else{
+    estado = "Persona Deshabilitada";
+  }
+
+  //Use Effect
+  const [contador, setContar] = useState(0);
+
+  useEffect(
+      ()=>{
+        console.log("Click numero:"+contador);
+        
+      } ,
+      [contador]
+  )
+
+  const contar = () => {
+    setContar(contador+1);
+  }
+
   //const v_nombre = "Juan Jose";
   //const v_apellido = "Lopez Perez"
   const o_persona = {
-      nombre: "Juan Jose",
+      nombre: "Jose Perez",
       apellido: "Lopez Perez",
       edad: 20,
       nacionalidad : "Hondurena",
@@ -17,25 +43,26 @@ function App() {
     alert("Hola Humano, soy "+ props);
   }
 
+  const f_habilitar = () =>{  
+    setEstadoPersona(!estadoPersona); 
+  }
+
   return (
     <div className="App">
       <header className="App-header">  
-      <Persona 
-          nombre   ="Juan"
-          apellido ="Perez"
-          edad     ="45"
-          nacionalidad = "Hondureña"
-          genero      = "M"
-          fnsaludar = {f_saludar}
-      /> 
+
+      <h2>La persona esta {estado}</h2>
+      <button onClick={f_habilitar}>Cambiar estado</button>
+
+      <button onClick={contar}>Contador</button>
+      <h3> Clicks {contador}</h3>
+
       <Persona 
           personaInfo ={o_persona}
           fnsaludar = {f_saludar}
       />   
 
-      <Persona  
-          fnsaludar = {f_saludar}
-      />   
+       
       </header>  
     </div>
   );
--- Ejercicios React/proyecto1/src/components/Persona.js
+++ Ejercicios React/proyecto1/src/components/Persona.js
@@ -2,29 +2,12 @@
 import React from "react";
 
 export default function Persona(props){
-    var nombre; 
-    var apellido;
-    var edad;
-    var nacionalidad;
-    var genero;    
     
-    //iniciar variables locales con variables del props
-    nombre   = props.nombre; 
-    apellido = props.apellido;
-    edad     = props.edad;
-    nacionalidad = props.nacionalidad;
-    genero   = props.genero;   
-
-    //iniciar variables con objeto del props
-    console.log(props.personaInfo);
-    if(props.personaInfo != undefined){
-        nombre   = props.personaInfo.nombre; 
-        apellido = props.personaInfo.apellido;
-        edad     = props.personaInfo.edad;
-        nacionalidad = props.personaInfo.nacionalidad;
-        genero   = props.personaInfo.genero;  
-    }
-
+    //Asignacion por destructuring
+    const {personaInfo, fnsaludar} = props;
+    const {nombre = "No disponible", apellido = "No disponible"} = personaInfo;  //props por defecto
+    
+    console.log(nombre); 
 
     return(
         <div>
@@ -33,11 +16,11 @@ export default function Persona(props){
                 <tbody>
                     <tr> <td>Nombre:    </td>   <td> {nombre} </td>      </tr>
                     <tr> <td>Apellidos: </td>   <td> {apellido}</td>  </tr>
-                    <tr> <td>edad:      </td>   <td> {edad}</td>  </tr>
-                    <tr> <td>nacionalidad:</td> <td> {nacionalidad}</td>  </tr>
-                    <tr> <td>genero:      </td> <td> {genero}</td>  </tr>
+                    <tr> <td>edad:      </td>   <td> {personaInfo.edad}</td>  </tr>
+                    <tr> <td>nacionalidad:</td> <td> {personaInfo.nacionalidad}</td>  </tr>
+                    <tr> <td>genero:      </td> <td> {personaInfo.genero}</td>  </tr>
                     <tr> <td>
-                            <button onClick={ () => props.fnsaludar(nombre) } >Saludar</button>
+                            <button onClick={ () => fnsaludar(personaInfo.nombre) } >Saludar</button>
                     </td> </tr>
                 </tbody>
             </table>
